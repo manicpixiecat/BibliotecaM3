25999c1b908a8c18531cf1c7d320ab97bd772063
Tati Pires <78283974+manicpixiecat@users.noreply.github.com>
2023-07-31 19:07:28 -0300

    populando  tabelas do banco

---
+++ script/populacao.sql
@@ -0,0 +1,30 @@
+
+use biblioteca;
+INSERT INTO usuario (id_user, nome, email, datanascimento)
+VALUES (1, 'Isabella Knight', 'isabella.knight@email.com', '2003-01-01');
+INSERT INTO usuario (id_user, nome, email, datanascimento)
+VALUES (2, 'Sophia Hartman', 'sophi.heart@ex.com', '1994-03-20');
+INSERT INTO usuario (id_user, nome, email, datanascimento)
+VALUES (3, 'Alexander Chase', 'alexchaseti@ex.com', '1980-08-25');
+INSERT INTO usuario (id_user, nome, email, datanascimento)
+VALUES (4, 'Kai Jongin', 'smkai@ex.com', '1994-01-14');
+INSERT INTO usuario (id_user, nome, email, datanascimento)
+VALUES (5, 'Kim Hanbin', 'iconeksinger@ex.com', '1996-08-22');
+INSERT INTO livros (id_livro, titulo, autor, ano_lancamento)
+VALUES (1, 'O Mistério da Casa Abandonada', 'Marina Gomes', '2005-08-22');
+INSERT INTO livros (id_livro, titulo, autor, ano_lancamento)
+VALUES (2, 'Sunflower Fields', 'Olivia Montgomery', '1990-01-14');
+INSERT INTO livros (id_livro, titulo, autor, ano_lancamento)
+VALUES (3, '"Corvo Invisivel: O Segredo das Sombras', 'Ethan Blackwood', '2010-05-10');
+INSERT INTO livros (id_livro, titulo, autor, ano_lancamento)
+VALUES (4, 'O Caminho para a Magia', 'Benjamin Brooks', '1993-11-06');
+INSERT INTO livros (id_livro, titulo, autor, ano_lancamento)
+VALUES (5, 'The Forgotten Realm', 'Mia Roberts', '2023-08-12');
+
+
+
+
+
+
+
+
