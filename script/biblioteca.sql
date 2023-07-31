78bb77ade7e3c9e1e70c4cfbcb02044f4d4a43ce
Tati Pires <78283974+manicpixiecat@users.noreply.github.com>
2023-07-31 18:15:42 -0300

    atualizado scripts da biblioteca.sql

--- script/biblioteca.sql
+++ script/biblioteca.sql
@@ -1,11 +1,30 @@
 use biblioteca;
-CREATE TABLE emprestimos (
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  data_emprestimo datetime NOT NULL,
-  data_devolucao datetime NOT NULL,
-  id_livro_fk INT,
-  id_user_fk INT,
-  FOREIGN KEY (id_livro_fk) REFERENCES livros(id_livro),
-  FOREIGN KEY (id_user_fk) REFERENCES usuario(id_user)
+create database biblioteca;
+use biblioteca;
+CREATE TABLE usuario (
+  id_user int NOT NULL AUTO_INCREMENT,
+  nome varchar(60),
+  email varchar(35),
+  datanascimento date,
+  PRIMARY KEY (id_user)
+);
+CREATE TABLE categoria (
+  id_categoria int NOT NULL,
+  genero varchar(35),
+  PRIMARY KEY (id_categoria)
+);
+CREATE TABLE livros (
+  id_livro int PRIMARY KEY,
+  titulo varchar(100) NOT NULL,
+  autor varchar(100),
+  ano_lancamento date,
+  id_categoria_fk int,
+  FOREIGN KEY (id_categoria_fk) REFERENCES categoria (id_categoria)
+);
+CREATE TABLE leitura (
+id int not null,
+id_livro_fk int,
+pag_atual int,
+FOREIGN KEY (id_livro_fk) REFERENCES livros (id_livro)
 );
 
